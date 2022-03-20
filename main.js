c63a001479a28218b9fe42969ee5493907ef7fd7
Kirill <RillKoff@yandex.ru>
2022-03-20 17:20:17 +0300

    Запрос ввода желаемого сдвига и создание цикла для сдвига. Работа закончена.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const strMassive = prompt('Введите элементы массива через пробел')
-
+const sdvig = prompt('Введите желаемый сдвиг')
 const massive = strMassive.split('')
 
 massive.forEach((item, index, array) => {
@@ -7,3 +7,8 @@ if (item === '' || item === ' ') {
 array.splice(index, 1)
 }
 });
+for(i = 0; i < sdvig; i++){
+massive.unshift(massive.pop())
+};
+
+alert(massive)
